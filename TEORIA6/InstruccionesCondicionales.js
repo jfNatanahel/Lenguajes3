a8acc649ae177ee816aadc0c83308270c922bad3
jfNatanahel <natanahel.jf@gmail.com>
2025-05-16 12:43:23 -0300

    TEORIA_CLASE6

---
+++ TEORIA6/InstruccionesCondicionales.js
@@ -0,0 +1,17 @@
+//IF
+var edad = prompt("Ingresar edad: ")
+if (edad>=16){
+    alert("Podes votar");
+}else{
+    alert("No podes votar! :(")
+}
+
+//if-else
+var nota = prompt("Ingresar tu nota: ");
+if (nota>=9){
+    alert("Excelente")
+}else if(nota>=7 && nota<9){
+    alert("Muy bien")
+}else{
+    alert("REPROBADO")
+}
---
+++ TEORIA6/codigo.js
@@ -0,0 +1,15 @@
+//VARIABLES
+var x,y; 
+
+//VARIABLES DINAMICAS
+var a = 3+2; //La variable a vale 5
+var b = a*2 //5*2 = 10
+a < b; //true 
+
+//ENTRADA Y SALIDA DEL USUARIO CON EL NAVEGADOR
+var x  = prompt("Texto a escribir")
+
+alert(x) //Mostrar un promp de lo que escribi anteriormente.
+console.log(x) //Muestra el mensaje en la consola.
+
+//INSTRUCCIONES CONDICIONALES
